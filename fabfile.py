00cf4d325f8188919c6cd045e55e5c31360190e3
Davi Campos <arlocm@gmail.com>
2012-10-30 19:42:02 -0200

    Start commit with def to set hostname

---
+++ fabfile.py
@@ -0,0 +1,16 @@
+from fabric.api import local, run, put, sudo
+
+# set(
+#     project = 'project_name',
+ 
+#     package     = '$(project).zip',
+ 
+#     # Remote servers
+#     fab_user    = 'usuario_ssh',
+#     fab_hosts   = ['servidor1.com', 'servidor2.com'],
+# )
+ 
+
+def set_hostname(host):
+    sudo('echo -e '+host+' > /etc/hostname')
+    sudo('echo -e "127.0.0.1\t'+host+'" >> /etc/hosts')
