10f5f69cad494326f0aa83545eb92a3303431f48
Davi Campos <arlocm@gmail.com>
2012-10-31 20:58:36 -0200

    Adding fabric to run fab to accept agent in master

--- fabfile.py
+++ fabfile.py
@@ -1,33 +1,42 @@
-from fabric.api import local, run, put, sudo
+from fabric.api import local, sudo, settings
 
 def set_hostname(host):
     sudo('echo -e "127.0.0.1\t'+host+'" >> /etc/hosts')
     sudo('hostname '+host)
 
-def set_puppet_master_host(master):
-    sudo('echo -e "' + master + '\tpuppet" >> /etc/hosts')
-
-def add_puppet_repo():
+def add_puppet_repository():
     sudo('echo -e "deb http://apt.puppetlabs.com/ precise main\ndeb-src http://apt.puppetlabs.com/ precise main" >> /etc/apt/sources.list.d/puppet.list')
     sudo('apt-key adv --keyserver keyserver.ubuntu.com --recv 4BD6EC30')
     sudo('apt-get update')
 
-def connect_to_master():
-    sudo('puppet agent --test')
+def agent_add_master_in_hosts(master):
+    sudo('echo -e "' + master + '\tpuppet" >> /etc/hosts')
 
-def install_puppet(host, master):
+def agent_connect_to_master(agent_host, master):
+    with settings(warn_only=True):
+        sudo('puppet agent --test')
+    local('fab -H ' + master + ' master_accept_agent:' + agent_host) 
+
+def agent_enable_autostart():
+    sudo('sed -i -re "s/START=no/START=yes/" /etc/default/puppet')
+    sudo('service puppet start')
+
+def puppet_agent_install(host, master):
 
     set_hostname(host)
-    add_puppet_repo()
-    set_puppet_master_host(master)
-
+    add_puppet_repository()
+    agent_add_master_in_hosts(master)
     sudo('apt-get install puppet')
+    agent_connect_to_master(host, master)
+    agent_enable_autostart()
 
-    connect_to_master()
 
-def install_puppet_master(host):
+def master_accept_agent(agent_host):
+    sudo('puppet cert sign ' + agent_host)
+
+def puppet_master_install(host):
 
     set_hostname(host)
-    add_puppet_repo()
+    add_puppet_repository()
     
     sudo('apt-get install puppetmaster') 
